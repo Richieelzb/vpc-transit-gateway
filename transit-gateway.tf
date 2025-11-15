fcfdcbaff2ed3b638725db6d8bca90cd21e525bb
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-11-16 01:36:32 +0200

    create

--- transit-gateway.tf
+++ transit-gateway.tf
@@ -1,20 +1,44 @@
-/*module "tgw" {
-  source  = "terraform-aws-modules/transit-gateway/aws"
-  name    = "lzb-tgw"
-  description = "TGW shared with other accounts"
+# 1. Create Transit Gateway
+resource "aws_ec2_transit_gateway" "tgw-lzb" {
+  description = "lzb Transit Gateway"
+  amazon_side_asn = 64512
+  auto_accept_shared_attachments = "enable"
+  default_route_table_association = "enable"
+  default_route_table_propagation = "enable"
+}
 
-  vpc_attachments = {
-    vpc1 = {
-      vpc_id     = var.tgw-vpc1
-      subnet_ids = var.sube
-      dns_support = true
-      ipv6_support = false
-      tgw_routes = [
-        { destination_cidr_block = "10.0.0.0/16" }
-      ]
-    }
-  }
+# 2. Attach VPCs to TGW
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc1_attach" {
+  transit_gateway_id = aws_ec2_transit_gateway.tgw-lzb.id
+  vpc_id             = module.VPC-A.vpc_id
+  subnet_ids         = [module.VPC-A.private_subnets]
+}
 
-  ram_allow_external_principals = true
-  ram_principals = [123456789012]
-}*/
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc2_attach" {
+  transit_gateway_id = aws_ec2_transit_gateway.tgw-lzb.id
+  vpc_id             = module.VPC-B.vpc_id
+  subnet_ids         = [module.VPC-B.private_subnets]
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc3_attach" {
+  transit_gateway_id = aws_ec2_transit_gateway.tgw-lzb.id
+  vpc_id             = module.VPC-C.vpc_id
+  subnet_ids         = [module.VPC-C.private_subnets]
+}
+
+
+# 3. Propagate routes to default TGW route table
+resource "aws_ec2_transit_gateway_route_table_propagation" "vpc1_propagation" {
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.vpc1_attach.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw-lzb.association_default_route_table_id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "vpc2_propagation" {
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.vpc2_attach.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw-lzb.association_default_route_table_id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "vpc3_propagation" {
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.vpc3_attach.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw-lzb.association_default_route_table_id
+}
