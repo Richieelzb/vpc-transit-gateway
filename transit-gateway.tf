17131dc880a876e0332901993a16fbb37581f2b9
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-02-25 15:31:20 +0200

    creating 2

--- transit-gateway.tf
+++ transit-gateway.tf
@@ -3,8 +3,9 @@ resource "aws_ec2_transit_gateway" "tgw-lzb" {
   description = "lzb Transit Gateway"
   amazon_side_asn = 64512
   auto_accept_shared_attachments = "enable"
-  default_route_table_association = "enable"
-  default_route_table_propagation = "enable"
+  default_route_table_association = "disable"
+  default_route_table_propagation = "disable"
+
 }
 
 # 2. Attach VPCs to TGW
@@ -29,16 +30,16 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "vpc3_attach" {
 
 # 3. Propagate routes to default TGW route table
 resource "aws_ec2_transit_gateway_route_table_propagation" "vpc1_propagation" {
-  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.vpc1_attach.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw-lzb.association_default_route_table_id
+  transit_gateway_attachment_id = [aws_ec2_transit_gateway_vpc_attachment.vpc1_attach.id,aws_ec2_transit_gateway_vpc_attachment.vpc2_attach.id]
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw-lzb.id
 }
 
 resource "aws_ec2_transit_gateway_route_table_propagation" "vpc2_propagation" {
   transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.vpc2_attach.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw-lzb.association_default_route_table_id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw-lzb.id
 }
 
 resource "aws_ec2_transit_gateway_route_table_propagation" "vpc3_propagation" {
   transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.vpc3_attach.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw-lzb.association_default_route_table_id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw-lzb.id
 }
